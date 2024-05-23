68f02fb9d27c79eaa0a2b3f5e9bc8e180a3df0dc
jinbokk <eyelash1024@naver.com>
2024-05-23 23:55:13 +0900

    [ADD] react-query 세팅 완료

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
-import GlobalStyles from '../styles/GlobalStyles'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '@/styles/base/font.css';
+import '@/styles/base/reset.css';
+import '@/styles/globals.css';
+import GlobalStyles from '../styles/GlobalStyles';
 
 interface AppProps {
   Component: any;
   pageProps: any;
 }
-const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <GlobalStyles />
-    <Component {...pageProps} />
-  </>
-)
+const App = ({ Component, pageProps }: AppProps) => {
+  const queryClient = new QueryClient();
 
-export default App
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+};
+export default App;
