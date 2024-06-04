607c49f842e3d8c7c52c2e90ed4805b15b583c7b
jinbokk <eyelash1024@naver.com>
2024-06-05 02:45:54 +0900

    [MOD] globalStyle body 설정 추가

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,8 +1,14 @@
 import { Global } from '@emotion/react';
-import tw, { GlobalStyles as BaseStyles, css } from 'twin.macro';
+import tw, { GlobalStyles as BaseStyles, css, theme } from 'twin.macro';
 
 const customStyles = css`
   body {
+    position: relative;
+    width: 100%;
+    min-width: ${theme`variables.apps.min-width`};
+    background-color: white;
+    -webkit-tap-highlight-color: transparent;
+    overflow: auto;
     font-family: 'Nanum Gothic', 'Pretendard', Helvetica, 'Apple SD Gothic Neo',
       Sans-serif !important;
     ${tw`antialiased`}
