79cce1819222f59aa29647251b9d0d18afb78321
jinbokk <eyelash1024@naver.com>
2024-07-19 02:11:09 +0900

    [MOD] 로그인 리다이렉트 페이지 처리

--- src/pages/social_login_handler/index.tsx
+++ src/pages/social_login_handler/index.tsx
@@ -1,4 +1,14 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
 // 리다이렉트 후 처리 페이지
-export default function social_login_handler() {
-  return <div></div>;
+export default function SocialLoginHandlerPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <></>;
 }
